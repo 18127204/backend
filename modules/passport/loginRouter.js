4f78a0039be11452bcd0cf80190a613a82c73574
18127024 <quangminhtran323@gmail.com>
2022-01-13 01:55:05 +0700

    he

--- modules/passport/loginRouter.js
+++ modules/passport/loginRouter.js
@@ -1,8 +1,11 @@
-var express=require('express');
-var router=express.Router();
-var passport=require('./index');
-var jwt = require('jsonwebtoken');
-
+// var express=require('express');
+// var router=express.Router();
+// var passport=require('./index');
+// var jwt = require('jsonwebtoken');
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const passport = require('./index');
+const router = express.Router();
 router.post('/',passport.authenticate('local',{session:false}),(req, res, next)=>{
     res.json({
         content:req.user,
