9c7dee7ff5cdf1efc4cd1343998a250bffd2507c
18127024 <quangminhtran323@gmail.com>
2022-01-13 01:20:39 +0700

    he

--- modules/passport/index.js
+++ modules/passport/index.js
@@ -8,17 +8,17 @@ var JwtStrategy = require('passport-jwt').Strategy,
 
 
 passport.use(new LocalStrategy(
-    {
-        usernameField: 'username',
-        passwordField: 'password',
-        passReqToCallback: true,
-        session:false
+    // {
+    //     usernameField: 'username',
+    //     passwordField: 'password',
+    //     passReqToCallback: true,
+    //     session:false
 
-    },
+    // },
     function (req, username, password, done) {
-        username = req.body.username;
-        password = req.body.password;
-        let sqlAccount = `select id,username,isadmin from account where username=? and password =? and emailVerify='' and lockacc=0`;
+        // username = req.body.username;
+        // password = req.body.password;
+        // let sqlAccount = `select id,username,isadmin from account where username=? and password =? and emailVerify='' and lockacc=0`;
         return done(null, false, { message: 'Incorrect username or password.' });
         // pool.query(sqlAccount, [username,password], (error, result) => {
         //     if (error) {               
